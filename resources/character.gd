05c621b4e4211c86e1f0e666869ef2875c1fbe2c
Victor <zerasul@gmail.com>
2025-07-15 21:10:53 +0200

    adding resources and some scenes

---
+++ resources/character.gd
@@ -0,0 +1,14 @@
+class_name Character
+extends Resource
+
+@export
+var animations:Resource
+
+@export_category("Stats")
+
+@export
+var power:int
+@export
+var speed: int
+@export
+var max_health: int
---
+++ scenes/player/player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export_category("Resource")
+@export
+var character:Character
+
+@export_category("Player stats")
+@export
+var health:int
+
+@export
+var score:int
+
+@export
+var special:int
+
+var attack_combo:int = 0
